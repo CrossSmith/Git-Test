e2ad2ed02f4abbaadb36ca38deeb5edea2c3b002
precsmit <precsmit@CSG1-522522>
2018-09-03 21:39:31 -0500

    This is my finished product.

--- practice1/Program.cs
+++ practice1/Program.cs
@@ -4,13 +4,51 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace practice1
-{
-    class Program
-    {
+namespace practice1 {
+    class Program {
         static void Main(string[] args) {
 
-            Console.WriteLine("Hello World");
+            Random num = new Random();
+
+            int correctNum = num.Next(0, 100);
+
+            bool win = false;
+            int counter = 0;
+
+            do
+            {
+                Console.Write("Guess a number between 0 and 100:  ");
+                string s = Console.ReadLine();
+
+                int guess = int.Parse(s);
+
+                if (guess > correctNum)
+                {
+                    Console.WriteLine("Your number is too high. Please try again.");
+                    counter++;
+                }
+                else if (guess < correctNum)
+                {
+                    Console.WriteLine("Your number is too low. Please try again.");
+                    counter++;
+                }
+                else if (guess == correctNum)
+                {
+                    Console.WriteLine("That is correct!");
+                    Console.WriteLine();
+                    Console.Write("It took you ");
+                    Console.Write(counter);
+                    Console.Write(" tries.");
+                    win = true;
+                }
+
+                Console.WriteLine();
+
+            } while (win == false);
+
+            //Console.WriteLine("Thank you for playing");
+
+
             Console.ReadKey();
         }
     }
